d713c03485205954e1e842216135e5cf31c17ee2
huangtengfei <huangtengfei@navinfo>
2021-07-30 11:27:02 +0800

    新增 spring-boot-demo

---
+++ spring-boot-demo/src/test/java/com/tenfi/springbootdemo/SpringBootDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.tenfi.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("测试spring");
+    }
+
+}
